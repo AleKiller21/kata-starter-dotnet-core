11605cb929beb8664a75fab6dba59b0ec3500804
Ferenck Vargas <ferenck@acklenavenue.com>
2019-09-03 11:48:30 -0600

    Custom delimiter passing, testing for one negative

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 
 namespace Kata
@@ -11,7 +12,17 @@ namespace Kata
                 return 0;
             }
 
-            var numbers = userInput.Split(new []{',', '\n'}).Select(int.Parse);
+            var delimiters = new []{",", "\n"};
+            var input = userInput;
+
+            if (userInput.StartsWith("//"))
+            {
+                var parts = input.Split('\n');
+                delimiters = new[] {parts.First().Replace("//", "")};
+                input = parts.Last();
+            }
+            
+            var numbers = input.Split(delimiters, StringSplitOptions.None).Select(int.Parse);
             return numbers.Sum();
 
         }
