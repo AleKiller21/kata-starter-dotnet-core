40b304c791fd3b0670b396a9a8177824b3a1bf88
Ferenck Vargas <ferenck@acklenavenue.com>
2019-09-03 11:36:39 -0600

    Unknown amount of numbers passing, testing for new line as delimiter

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -11,14 +11,8 @@ namespace Kata
                 return 0;
             }
 
-            var numbers = userInput.Split(',');
-            if (numbers.Length == 1)
-            {
-                return int.Parse(numbers[0]);
-            }
-
-            var nums = numbers.Select(int.Parse).ToArray();
-            return nums[0] + nums[1];
+            var numbers = userInput.Split(',').Select(int.Parse);
+            return numbers.Sum();
 
         }
     }
