42e46c0953c1f76c0e97e3939468efd18e341fa6
Ferenck Vargas <ferenck@acklenavenue.com>
2019-09-06 14:03:27 -0600

    Refactor Add function

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -12,34 +12,72 @@ namespace Kata
                 return 0;
             }
             
-            var delimiters = new[] {",", "\n"};
-            var newInput = userInput;
+            var newInput = GetInput(userInput);
 
-            if (userInput.StartsWith("//"))
+            var delimiters = GetDelimiters(userInput);
+            
+            var numbers = ParseNumbers(newInput, delimiters);
+            
+            ValidateNegatives(numbers);
+
+            return numbers.Sum();
+        }
+
+        static string GetInput(string userInput)
+        {
+            if (HasCustomDelimiter(userInput))
             {
-                var parsedInput = userInput.Split('\n');
-                var delimiterSection = parsedInput.First();
-                var delimiter = delimiterSection
+                var parsedInput = GetCustomDelimiterParts(userInput);
+                return parsedInput.Last();
+            }
+
+            return userInput;
+        }
+
+        static string[] GetCustomDelimiterParts(string userInput)
+        {
+            return userInput.Split('\n');
+        }
+
+        static bool HasCustomDelimiter(string userInput)
+        {
+            return userInput.StartsWith("//");
+        }
+
+        static string[] GetDelimiters(string userInput)
+        {
+            var delimiters = new[] {",", "\n"};
+            if (HasCustomDelimiter(userInput))
+            {
+                var delimiter = GetCustomDelimiterParts(userInput)
+                    .First()
                     .Replace("//", "")
                     .Replace("[", "")
-                    .Replace("]", "");
-                delimiters = new[] {delimiter};
-                newInput = parsedInput.Last();
+                    .Split("]");
+                delimiters = delimiter;
             }
-            
-            var numbers = newInput
+
+            return delimiters;
+        }
+
+        static int[] ParseNumbers(string input, string[] delimiters)
+        {
+            var numbers = input
                 .Split(delimiters, StringSplitOptions.None)
                 .Select(int.Parse)
                 .Where(n => n <= 1000)
                 .ToArray();
+            return numbers;
+        }
+
+        static void ValidateNegatives(int[] numbers)
+        {
             var negatives = numbers.Where(n => n < 0).ToArray();
 
             if (negatives.Any())
             {
                 throw new Exception($"negatives not allowed: {string.Join(", ", negatives)}");
             }
-
-            return numbers.Sum();
         }
     }
 }
