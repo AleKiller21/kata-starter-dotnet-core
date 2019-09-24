5faab5b4fe91bd48629524d9b98c12f2690c9f56
Alejandro Ferrera <alejandro.ferrera@outlook.com>
2019-09-24 15:26:22 -0600

    Add spec for when the user input contains a negative number and an exception should be thrown

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -12,15 +12,21 @@ namespace Kata
                 return 0;
             }
 
-            var numbers = userInput
-                .Split(new []{",", "\n"}, StringSplitOptions.None)
+            var delimiters = new[] {",", "\n"};
+            var values = userInput;
+            if (userInput.StartsWith("//"))
+            {
+                var parsedInput = userInput.Split('\n');
+                var delimiterSection = parsedInput.First();
+                values = parsedInput.Last();
+                var delimiter = delimiterSection.Replace("//", "");
+                delimiters = new[] {delimiter};
+            }
+
+            var numbers = values
+                .Split(delimiters, StringSplitOptions.None)
                 .Select(int.Parse)
                 .ToArray();
-
-            if (numbers.Length == 1)
-            {
-                return numbers.First();
-            }
 
             return numbers.Sum();
         }
