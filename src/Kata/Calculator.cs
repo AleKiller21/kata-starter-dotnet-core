d6bc1a6bef1b6df216420f65ff41608ca0b0ba4c
Alejandro Ferrera <alejandro.ferrera@outlook.com>
2019-10-25 15:57:29 -0600

    Add spec to avoid numbers larger than 1000

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -31,7 +31,7 @@ namespace Kata
 
             if (negatives.Any())
             {
-                throw new Exception($"negatives not allowed: {negatives.First()}");
+                throw new Exception($"negatives not allowed: {string.Join(", ", negatives)}");
             }
 
             return numbers.Sum();
